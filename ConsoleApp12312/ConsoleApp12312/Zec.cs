28046c18905caffa6a269c92a07fa56eb5a8436d
NikolaLugumerski <nikolalugumerski123@gmail.com>
2022-03-17 12:00:39 +0100

    Created model for Zec

---
+++ ConsoleApp12312/ConsoleApp12312/Zec.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12312
+{
+	internal class Zec
+	{
+
+		public Zec()
+		{
+				
+		}
+
+		public string noge { get; set; }
+		public int MyProperty { get; set; }
+	}
+
+}
